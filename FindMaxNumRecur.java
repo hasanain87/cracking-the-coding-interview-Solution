68c15100bbae5ce132686319ff74cf529b266fb6
hasanain87 <AL_SABONCHI_HASANAIN@student.smc.edu>
2019-01-02 15:49:49 -0800

    Update FindMaxNumRecur.java

--- FindMaxNumRecur.java
+++ FindMaxNumRecur.java
@@ -1,28 +1,29 @@
 public class FindMaxNum {
    
       static int start=0;
-      static int max;
+	    static int[] arr={90,70,100,30,101};
+	    static int  max=arr[0];
+	
    
-   static int max (int[] nums){
-      
-            //Start is an array flag 
-            if(start ==nums.length-1) return max;
-            else
-               
-            if (nums[start] >start)  max=nums[start];
-
-                start++;
-            // call the function again ,but with an updated start point
-            return  max(nums);
-
-      
-     }
-   
+   public static int maxNum (int[] arr,int end){
+	   
+	      
+	     if(start >end) return max;
+	     else
+	         if(arr[start] > max) {
+	         max=arr[start];
+	         start++;
+           maxNum( arr,end);
+	         } 
+	     start++;
+	         return  maxNum( arr,end);
+	      }
+	      
    public static void main(String[] args) {
       
-      int []nums={1,7,9,19,90,5};
-      
-            max(nums);
-      
+     
+	      int length=arr.length-1;
+	      System.out.println(maxNum(arr,length));
+	      
    }
 }
