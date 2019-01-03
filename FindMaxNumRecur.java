bfa52c929526e7abf2bfd7c5e6663b49477f8595
hasanain87 <AL_SABONCHI_HASANAIN@student.smc.edu>
2019-01-02 16:12:52 -0800

    Update FindMaxNumRecur.java

--- FindMaxNumRecur.java
+++ FindMaxNumRecur.java
@@ -19,11 +19,32 @@ public class FindMaxNum {
 	         return  maxNum( arr,end);
 	      }
 	      
+	
+	
+	public static int pow2 (int exp){
+	   
+      if (exp==0) return 1; // base case
+      else 
+         return 2*pow2(exp-1);
+	   
+	 }
+	
+	public static int SumAll (int num){
+	   
+	   if (num==0) return 0;
+      else 
+         return num+SumAll(num-1);
+	   
+	      }
+	
+	
+	
    public static void main(String[] args) {
       
      
 	      int length=arr.length-1;
-	      System.out.println(maxNum(arr,length));
-	      
+	      System.out.println ("the max number in the array is  "maxNum(arr,length));
+	      System.out.println("2 to the power 4 is "+pow2(4));
+	      System.out.println("Sum of nums 1 to 1oo is "+SumAll(100));
    }
 }
